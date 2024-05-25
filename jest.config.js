09935f93b35e922b8f7f77cce13c36be63110d2d
Roman Shtylman <roman@foxglove.dev>
2024-05-24 18:16:13 -0700

    Add union and vector of unions support (#104)
    Something notable is that the `_type` fields are also present in the
    de-serialized object. I did not add a setting to control this behavior
    but that might be something desirable in the future.

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,11 @@
 module.exports = {
-  preset: "ts-jest",
   testMatch: ["<rootDir>/src/**/*.test.ts"],
+  transform: {
+    "^.+\\.tsx?$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+  },
 };

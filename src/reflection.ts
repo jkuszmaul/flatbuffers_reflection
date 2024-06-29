fd29cd2f50ab2f560d5b66ce6a0d209c6dfcb9e6
Roman Shtylman <roman@foxglove.dev>
2024-06-29 11:12:10 -0400

    remove fixme so tests run

--- src/reflection.ts
+++ src/reflection.ts
@@ -861,7 +861,6 @@ export class Parser {
     const unionDeserializers = this.createUnionDeserializers(fieldType, readDefaults);
 
     // Unions can only be formed from tables so we know our union field will point to a table
-    // fixme
     //const rawLambda = this.readTableLambda2(field);
     const rawLambda = this.readTableLambda(field, typeIndex);
 
